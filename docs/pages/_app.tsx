5e3501ee094c17f3e66a74d8d9984e150d8d4ee4
Ridwan Olanrewaju <olaitanlanre28@gmail.com>
2024-04-30 08:52:46 +0100

    Update favicon paths in _app.tsx

--- docs/pages/_app.tsx
+++ docs/pages/_app.tsx
@@ -12,41 +12,36 @@ export function FavIcon() {
         rel="icon"
         sizes="16x16"
         type="image/png"
-        href="/favicon/favicon-16x16.png"
+        href="/favicon-16x16.png"
       />
       <link
         rel="icon"
         sizes="32x32"
         type="image/png"
-        href="/favicon/favicon-32x32.png"
+        href="/favicon-32x32.png"
       />
-      <link
-        rel="icon"
-        sizes="48x48"
-        type="image/x-icon"
-        href="/favicon/favicon.ico"
-      />
+      <link rel="icon" sizes="48x48" type="image/x-icon" href="/favicon.ico" />
       <link
         rel="icon"
         type="image/png"
         sizes="192x192"
-        href="/favicon/android-chrome-192x192.png"
+        href="/android-chrome-192x192.png"
       />
       <link
         rel="icon"
         type="image/png"
         sizes="512x512"
-        href="/favicon/android-chrome-512x512.png"
+        href="/android-chrome-512x512.png"
       />
       <link
         rel="apple-touch-icon"
         sizes="180x180"
-        href="/favicon/apple-touch-icon.png"
+        href="/apple-touch-icon.png"
       />
       <link
         rel="manifest"
         crossOrigin="use-credentials"
-        href="/favicon/site.webmanifest"
+        href="/site.webmanifest"
       />
     </Head>
   );
