26f27feb2dd9dc5fde066fa3c81eed3348ccd77b
Ridwan Olanrewaju <olaitanlanre28@gmail.com>
2024-08-20 06:09:53 +0100

    chore: Update next.config.js with distDir change

--- docs/next.config.js
+++ docs/next.config.js
@@ -7,11 +7,15 @@ const withNextra = require("nextra")({
 module.exports = {
   ...withNextra(),
 
-  assetPrefix: "https://ibnlanre.github.io/builder/",
-
-  // https://nextjs.org/docs/app/api-reference/next-config-js/basePath
-  basePath: "",
+  // https://nextjs.org/docs/pages/api-reference/next-config-js/distDir
+  distDir: "_site",
 
   // https://nextjs.org/docs/app/building-your-application/deploying/static-exports
   output: "export",
+
+  // https://nextjs.org/docs/pages/api-reference/next-config-js/assetPrefix
+  // assetPrefix: "https://ibnlanre.github.io/builder/",
+
+  // https://nextjs.org/docs/app/api-reference/next-config-js/basePath
+  // basePath: "",
 };
