29a2351bfcbbf763f1561365f55045e37d2f233d
Ridwan Olanrewaju <olaitanlanre28@gmail.com>
2024-10-14 17:10:08 +0100

    Refactor: Update package.json version to 1.0.0

--- src/core/types/index.ts
+++ src/core/types/index.ts
@@ -1,7 +1,6 @@
 export type { Base } from "./Base";
 export type { Builder } from "./Builder";
 export type { Dictionary } from "./Dictionary";
-export type { Intersect } from "./Intersect";
 export type { Join } from "./Join";
 export type { KeyBuilder } from "./KeyBuilder";
 export type { Paths } from "./Paths";
